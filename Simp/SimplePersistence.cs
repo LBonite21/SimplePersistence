227344eb16a7af0d077c03809801420409aaa439
AnibalTR <aanibaltinoco@gmail.com>
2020-10-07 14:28:07 -0600

    got start with method

--- Simp/SimplePersistence.cs
+++ Simp/SimplePersistence.cs
@@ -17,49 +17,52 @@
             {
                 Selection(info.MenuSelection());
             }
-            
+
         }
 
         public bool Selection(int UserSelction)
         {
-           
-                switch (UserSelction)
-                {
-                    case 0:
-                        System.Console.WriteLine("Closing . . . . . .");
-                        ChoiceSelected = true;
-                        break;
-                    case 1:
-                        CreateEmployeeRecord();
-                        System.Console.WriteLine("yes 1");
-                        ChoiceSelected = true;
-                        break;
 
-                    case 2:
+            switch (UserSelction)
+            {
+                case 0:
+                    System.Console.WriteLine("Closing . . . . . .");
+                    ChoiceSelected = true;
+                    break;
+                case 1:
+                    PrintEmployee($@"D:\temp\people\simple\");
+                    System.Console.WriteLine("yes 1");
+                    ChoiceSelected = true;
+                    break;
 
-                        EmployeeSearch();
-                        System.Console.WriteLine("yes 2");
-                        ChoiceSelected = true;
-                        break;
+                case 2:
 
-                    default:
-                        System.Console.WriteLine("Please enter the number options of 1 or 2");
-                        break;
-                }
+                    PrintPeopleDetails($@"D:\temp\people\simple\");
+                    System.Console.WriteLine("yes 2");
+                    ChoiceSelected = true;
+                    break;
+
+                default:
+                    System.Console.WriteLine("Please enter the number options of 1 or 2");
+                    break;
+            }
 
             return ChoiceSelected;
 
 
         }
 
-        public void CreateEmployeeRecord()
+        public void PrintEmployee(string path)
         {
 
         }
 
-        public void EmployeeSearch()
+        public void PrintPeopleDetails(string path)
         {
-
+            //take that path
+            //grab the file split
+            //create a user with that info
+            //display it to the user
         }
     }
 }
