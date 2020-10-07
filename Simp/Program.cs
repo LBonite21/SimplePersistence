4f0daf6894c7b418294bebcf35a888a680816666
Lemuel Bonite <ljbonite21@gmail.com>
2020-10-07 08:36:22 -0600

    Added project solution

---
+++ Simp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Simp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
