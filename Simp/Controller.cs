0efde0bd658d3f8802cb63e96c68f1a7d97a42c6
AnibalTR <aanibaltinoco@gmail.com>
2020-10-07 09:48:34 -0600

    added some menu stuff

--- Simp/Controller.cs
+++ Simp/Controller.cs
@@ -7,9 +7,15 @@
             
         }
 
-        public void MenuSelection()
+        public int MenuSelection()
         {
-            System.Console.WriteLine("test does this work?");
+            System.Console.WriteLine();
+            System.Console.WriteLine("- Employee Info Stuff -\n  1: Create New Employee \n  2: Go Through Records");
+            System.Console.Write("Enter Choice: ");
+            int input = int.Parse(System.Console.ReadLine());
+            return input;
         }
+
+
     }
 }
